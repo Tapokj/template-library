6b2970219d4a505014dab9d5d17048331b9f5040
Dankonia <imacdavplay@gmail.com>
2020-07-28 10:54:27 +0300

    imports fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,8 @@
-import React, { HTMLAttributes, ReactChild } from 'react';
-
-import { Form, Field } from './Form';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
-}
-
-export const AppExample = () => {
-  return (
-    <Form
-      isLoader={false}
-      isLoading={false}
-      onSubmit={data => console.log('submit', data)}
-      initialValues={{ email: '' }}
-    >
-      <Field name="email" />
-    </Form>
-  );
-};
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: hps://github.com/storybookjs/storybook/issues/9556tt
-export const Thing = () => {
-  return <AppExample />;
-};
+export { Http } from './http';
+export { Form, Field } from './Form';
+export {
+  action,
+  createRequestTypes,
+  createActionAc,
+  fetchEntity,
+} from './redux-utils';
